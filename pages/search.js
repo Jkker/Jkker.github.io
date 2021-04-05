26dc2e833cfbb110b1c29b02891b6e876c61cde7
Jkker <jerryjia@nyu.edu>
2021-04-06 01:57:29 +0800

    feat: geoip/country integrated in metasearch engine switch

--- pages/search.js
+++ pages/search.js
@@ -9,7 +9,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React, { useCallback, useEffect, useRef, useState } from 'react'
-import request from 'umi-request'
+import useSWR from 'swr'
+
+const fetcher = (url) => fetch(url).then((r) => r.json())
 
 const DEBUG = false
 // Custom constructor hook; run once before render
@@ -21,6 +23,7 @@ function useConstructor(callBack = () => {}) {
 }
 
 export default function Search() {
+  const { data: geoData, error: geoError } = useSWR('/api/geoip/country', fetcher)
   // Switch theme to light on load
   const { theme, setTheme } = useTheme()
   if (theme === 'dark') {
@@ -36,22 +39,13 @@ export default function Search() {
   const [hasProxy, setHasProxy] = useState(false)
 
   // Detect if user has proxy & switch tabs accordingly
-  /*   useEffect(() => {
-    request
-      .get('/api/geoip')
-      .then(function (response) {
-        console.log(response)
-        const userHasProxy = response.country === 'CN' ? false : true
-        setHasProxy(userHasProxy)
-        const key = userHasProxy
-          ? frames('', userHasProxy)[0].title
-          : frames('', userHasProxy)[1].title
-        setDefaultEngine(key)
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
-  }, []) */
+  useEffect(() => {
+    console.log(geoData)
+    const userHasProxy = geoData?.country === 'CN' ? false : true
+    setHasProxy(userHasProxy)
+    const key = userHasProxy ? frames('', userHasProxy)[0].title : frames('', userHasProxy)[1].title
+    setDefaultEngine(key)
+  }, [geoData])
 
   //* Core search functionality
 
